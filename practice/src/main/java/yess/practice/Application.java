02669383b4db96dd56877ce73d9163ddda30502a
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-09-27 21:06:44 -0300

    add mvn project

---
+++ practice/src/main/java/yess/practice/Application.java
@@ -0,0 +1,8 @@
+package yess.practice;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("A commit has been made!");
+    }
+}
---
+++ practice/src/test/java/yess/practice/ApplicationTest.java
@@ -0,0 +1,13 @@
+package yess.practice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
